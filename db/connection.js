8f84293ee6d5ba5829d07eefa4acf96d952144d3
millires <eml_box@hotmail.com>
2025-03-26 14:26:59 +0000

    task-13-fixed

--- db/connection.js
+++ db/connection.js
@@ -4,12 +4,12 @@ const ENV = process.env.NODE_ENV || 'development'
 
 require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 
-const db = new Pool();
+//const db = new Pool();
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
     throw new Error("No PGDATABASE configured")
 } else { 
-    console.log(`Connected to ${process.env.PGDATABASE}, ${process.env.DATABASE_URL}`)
+    console.log(`--- Connected to ${process.env.PGDATABASE}, ${process.env.DATABASE_URL}`)
 }
 
 const config = {};
@@ -20,5 +20,8 @@ if (ENV === "production") {
     config.connectionString = process.env.DATABASE_URL;
     config.max = 2;
 }
-module.exports = new Pool(config);
+//module.exports = new Pool(config);
+const db = new Pool(config);
+//console.log('db ---- ', db)
+
 module.exports = db;
